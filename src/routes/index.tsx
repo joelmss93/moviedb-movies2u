1ba0baa21ae1065561c5bea7c00c213f7036bd5d
joelmss93 <joelmss93@gmail.com>
2020-11-30 21:05:49 -0300

    Creating routes and files

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Landing from '../pages/Landing';
+import MovieInfo from '../pages/MovieInfo';
+import Login from '../pages/Login';
+
+const routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Landing} />
+    <Route path="/movie/" component={MovieInfo} />
+    <Route path="/login" component={Login} />
+  </Switch>
+);
+
+export default routes;
